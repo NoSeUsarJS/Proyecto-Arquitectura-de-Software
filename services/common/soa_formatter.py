0966c817595513976ffc8d45c085c9680436ea17
NoSeUsarJS <113266693+NoSeUsarJS@users.noreply.github.com>
2024-06-04 19:41:10 -0400

    soa_formatter added

---
+++ services/common/soa_formatter.py
@@ -0,0 +1,19 @@
+from services_mapping import services_mapping
+
+def soa_formatter(service_name: str, content: str) -> str | None:
+    try:
+        service = services_mapping[service_name]
+    except KeyError:
+        print(f"ERROR: '{service_name}' does not exist.")
+        return None
+    
+    payload_length = len(service) + len(content)
+    padding_length = 5 - len(str(payload_length))
+    padding = ""
+
+    for _ in range(payload_length):
+        padding += "0"
+    
+    formatted_response = padding + str(payload_length) + service + content
+
+    return formatted_response
