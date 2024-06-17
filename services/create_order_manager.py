306928e88aa13d9358bd95b63406461081542a66
Javier Molina <javier.molina@mail.udp.cl>
2024-06-16 21:59:13 -0400

    servicio pedidos

--- services/create_order_manager.py
+++ services/create_order_manager.py
@@ -2,6 +2,7 @@ import json
 import socket
 from common.services import Service
 from common.services import soa_formatter
+from datetime import datetime
 
 def Enviar(query):
     server_address = ('localhost', 5001)
@@ -37,33 +38,52 @@ def Enviar(query):
         sock.close()
 
 # Función para manejar la solicitud del inventario
-def handle_inventory_request(data: str) -> str:
+def handle_order_action(data: str) -> str:
     data = json.loads(data)
     action = data.get('action')
     
     if action == "1":
-        nombre = data.get('nombre')
-        cantidad = data.get('cantidad')
-        print("Agregando ingredientes...")
-        query = f"INSERT INTO ingredientes ( nombre, cantidad_ingredientes) VALUES ({nombre},{cantidad})"
-        ver = Enviar(query)
-        if (ver == "null"):
-            print("Se realizo")
-        response = "Ingrediente añadido"
+        mesa = data.get('mesa')
+        rut = data.get('rut')
+        platillos = list(data.get('platillos'))
+        hora = datetime.now().time()
+        print("Agregando pedido...")
+        query = f"INSERT INTO pedido (mesa, hora, id_persona) VALUES ({mesa},'{hora}', (SELECT id FROM persona WHERE rut = '{rut}'))"
+        ver1 = True if Enviar(query) == "null" else False
 
-    elif action == "4":
-        query = f"SELECT * FROM ingredientes"
+        if not ver1:
+            response = "Error al añadir pedido"
+            print(response)
+            return response
+        
+        for platillo in platillos:
+            id_platillo , comentario = platillo.get('platillo'), platillo.get('comentario')
+            query = f"Insert INTO Relacion_Platillo_Pedido (id_platillo, id_pedido, Comentario) VALUES ({id_platillo}, (SELECT id_Pedido FROM pedido WHERE hora = '{hora}' AND mesa = {mesa}), '{comentario}')"
+            ver1 = True if Enviar(query) == "null" else False
+            if not ver1:
+                response = f"Error al añadir platillo {id_platillo}"
+                print(response)
+                return response
+
+        response = "Pedido añadido"
+
+    elif action == "2":
+        query = f"SELECT * FROM pedido"
         response = Enviar(query)
         
         
         matriz = json.loads(response)
+        print(matriz)
         lista = []
         for i in range(len(matriz)):
             id = matriz[i][0]
-            nombre = matriz[i][1]
-            cantidad = matriz[i][2]
-            lista.append(f" Id: {id} - Nombre: {nombre} - Cantidad: {cantidad}")
-        response = json.dumps(lista)
+            mesa = matriz[i][1]
+            hora = matriz[i][2]
+            id_persona = matriz[i][3]
+            lista.append(f" Id: {id} - Mesa: {mesa} - Hora: {hora} - ID persona: {id_persona}")
+        response = json.dumps({
+            "pedidos": lista
+        })
     else:
         response = "Acción no válida."
     
@@ -73,4 +93,4 @@ def handle_inventory_request(data: str) -> str:
 # "db_manager": "SV005",
 # Inicializar y ejecutar el servicio
 inventory_service = Service(service_name="create_order_manager", host="localhost", port=5001)
-inventory_service.run_service(handle_inventory_request)
+inventory_service.run_service(handle_order_action)
